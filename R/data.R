6c46f88832b45f54f355ff77c87de504aff7317f
Malcolm Barrett <malcolmbarrett@gmail.com>
2018-02-06 16:28:41 -0800

    update data documentation

--- R/data.R
+++ R/data.R
@@ -1,55 +1,51 @@
 #' @importFrom tibble tibble
 NULL
 
-#' Record of the Transmission of the Light
+#' The Blue Cliff Record
 #'
-#' A koan collection written in 1300 by Zen Master Keizan Jokin that describes
-#' the enlightenment experience of of the Buddha and the 53 ancestors that led
-#' to Keizan Zenki's school of Zen.
+#' A koan collection written in 1125 and compiled by by Zen Master Yuanwu Keqin
+#' that includes the 100 Verses on Old Cases, a compilation of 100 koans
+#' collected by Xuedou Chongxian (Setcho), whose commentary appear ocassionally
+#' throughout.
 #'
-#' @format A data frame with four variables: \code{collection}, \code{case},
+#' @format A \code{tbl} with four variables: \code{collection}, \code{case},
 #'   \code{type}, and \code{text}. \code{collection} is the title of the text,
-#'   "Record of the Transmission of the Light". \code{case} indicates the koan
-#'   case number. \code{type} indicates the text type, one of: "title", the
-#'   title of the case, "main_case", the main case of the koan, "commentary",
-#'   commentary on the koan by Keizan Zenji, or "capping_verse", a short verse
-#'   by Keizan Zenji summarizing the essence of the koan.
+#'   "Blue Cliff Record". \code{case} indicates the koan case number.
+#'   \code{type} indicates the text type, one of: "title", the title of the
+#'   case, "main_case", the main case of the koan, or "commentary", commentary
+#'   on the koan by Setcho.
 #'
-#' @source https://shastaabbey.org/publications/
+#' @source http://4x13.net/buddhism/koan_bluecliff.pdf
 "blue_cliff_record"
 
-#' Record of the Transmission of the Light
+#' Book of Equanimity
 #'
-#' A koan collection written in 1300 by Zen Master Keizan Jokin that describes
-#' the enlightenment experience of of the Buddha and the 53 ancestors that led
-#' to Keizan Zenki's school of Zen.
+#' A koan collection written in 1224 by Zen Master Wansong Xingxiu that is
+#' comprised of 100 koans written by 11th century Zen Master Hongzhi Zhengjue
 #'
-#' @format A data frame with four variables: \code{collection}, \code{case},
+#' @format A \code{tbl} with four variables: \code{collection}, \code{case},
 #'   \code{type}, and \code{text}. \code{collection} is the title of the text,
-#'   "Record of the Transmission of the Light". \code{case} indicates the koan
-#'   case number. \code{type} indicates the text type, one of: "title", the
-#'   title of the case, "main_case", the main case of the koan, "commentary",
-#'   commentary on the koan by Keizan Zenji, or "capping_verse", a short verse
-#'   by Keizan Zenji summarizing the essence of the koan.
+#'   "Book of Equanimity". \code{case} indicates the koan case number.
+#'   \code{type} indicates the text type, one of: "title", the title of the
+#'   case, or "main_case", the main case of the koan.
 #'
-#' @source https://shastaabbey.org/publications/
+#' @source http://www.thezensite.com/ZenTeachings/KoanStudies/Shoyoroku.pdf
 "book_of_equanimity"
 
-#' Record of the Transmission of the Light
+#' The Gateless Gate
 #'
-#' A koan collection written in 1300 by Zen Master Keizan Jokin that describes
-#' the enlightenment experience of of the Buddha and the 53 ancestors that led
-#' to Keizan Zenki's school of Zen.
+#' A koan collection written in 1228 by Zen Master Wumen Huikai (Japanese: Mumon
+#' Ekai) that includes a commentary and capping verse by Wumen for each koan.
 #'
-#' @format A data frame with four variables: \code{collection}, \code{case},
+#' @format A \code{tbl} with four variables: \code{collection}, \code{case},
 #'   \code{type}, and \code{text}. \code{collection} is the title of the text,
-#'   "Record of the Transmission of the Light". \code{case} indicates the koan
-#'   case number. \code{type} indicates the text type, one of: "title", the
-#'   title of the case, "main_case", the main case of the koan, "commentary",
-#'   commentary by Keizan Zenji, or "capping_verse", a short verse by Keizan
-#'   Zenji summarizing the essence of the koan.
+#'   "The Gateless Gate". \code{case} indicates the koan case number.
+#'   \code{type} indicates the text type, one of: "title", the title of the
+#'   case, "main_case", the main case of the koan, "commentary", commentary by
+#'   Wumen (Mumon), or "capping_verse", a short verse by Wumen (Mumon)
+#'   summarizing the essence of the koan.
 #'
-#' @source https://shastaabbey.org/publications/
+#' @source http://info.stiltij.nl/publiek/meditatie/koan/mumonkan-shimomisse.pdf
 "gateless_gate"
 
 #' Record of the Transmission of the Light
@@ -58,7 +54,7 @@ NULL
 #' the enlightenment experience of of the Buddha and the 53 ancestors that led
 #' to Keizan Zenki's school of Zen.
 #'
-#' @format A data frame with four variables: \code{collection}, \code{case},
+#' @format A \code{tbl} with four variables: \code{collection}, \code{case},
 #'   \code{type}, and \code{text}. \code{collection} is the title of the text,
 #'   "Record of the Transmission of the Light". \code{case} indicates the koan
 #'   case number. \code{type} indicates the text type, one of: "title", the
